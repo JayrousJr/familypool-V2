a990e8fd0dadb16811310e92d58c7f1873c44e3c
Joshua Jayrous <joshuajayrous@gmail.com>
2024-07-18 16:19:18 +0300

    policy fixing

---
+++ app/Policies/AssignedTasksPolicy.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\AssignedTasks;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class AssignedTasksPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        if ($user->isManager() || $user->isTechnician()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, AssignedTasks $assignedTasks): bool
+    {
+        if ($user->isManager() || $user->isTechnician()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        if ($user->isManager() || $user->isTechnician()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, AssignedTasks $assignedTasks): bool
+    {
+        if ($user->isManager() || $user->isTechnician()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, AssignedTasks $assignedTasks): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, AssignedTasks $assignedTasks): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, AssignedTasks $assignedTasks): bool
+    {
+        return false;
+    }
+}
