00b969b410f18263be7ce9901c75b320bb585940
Joshua Jayrous <joshuajayrous@gmail.com>
2024-07-18 19:52:25 +0300

    fixing widgets

--- app/Filament/Resources/TaskResource/Pages/ListTasks.php
+++ app/Filament/Resources/TaskResource/Pages/ListTasks.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\TaskResource\Pages;
 
 use App\Filament\Resources\TaskResource;
+use App\Filament\Widgets\TasksOverview;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -14,6 +15,12 @@ class ListTasks extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            TasksOverview::class,
         ];
     }
 }
